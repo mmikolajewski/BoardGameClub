bf53800a3e2d713e506c5dd993c6e586cd70d316
Mikolaj Mikolajewski <mikolaj.jewski@gmail.com>
2023-09-11 20:31:41 +0200

    prod.yml and server.yml changes

--- build-and-restart.bat
+++ build-and-restart.bat
@@ -1,5 +1,5 @@
 docker build -t pawn .
 docker stop pawn
 docker rm pawn
-docker run -p 8080:8080 -v C:\Users\mikol\uploads\img:/uploads/img/  --name=pawn -e SPRING_PROFILES_ACTIVE=prod,docker --name pawn pawn
+docker run -p 8080:8080 -v C:\Users\mikol\uploads\img:/uploads/img/  --name=pawn -e SPRING_PROFILES_ACTIVE=prod,server --name pawn pawn
 
